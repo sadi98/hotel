ae0294f3ce867f894fb77e711a49755841db8dec
sadi nur sholeihin <sadynursholihin@gmail.com>
2026-08-18 00:22:14 +0700

    template ui admin

--- resources/views/admin/layouts/scripts.blade.php
+++ resources/views/admin/layouts/scripts.blade.php
@@ -1,4 +1,12 @@
-<script src="
-https://cdn.jsdelivr.net/npm/sweetalert2@11.26.25/dist/sweetalert2.all.min.js
-"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
+</script>
+<script src="{{ asset('admin/js/scripts.js') }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js" crossorigin="anonymous"></script>
+<script src="{{ asset('admin/assets/demo/chart-area-demo.js') }}"></script>
+<script src="{{ asset('admin/assets/demo/chart-bar-demo.js') }}"></script>
+<script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js"
+    crossorigin="anonymous"></script>
+<script src="{{ asset('admin/js/datatables/datatables-simple-demo.js') }}"></script>
+<script src="https://cdn.jsdelivr.net/npm/litepicker/dist/bundle.js" crossorigin="anonymous"></script>
+<script src="{{ asset('admin/js/litepicker.js') }}"></script>
 @stack('script')
--- routes/admin.php
+++ routes/admin.php
@@ -2,17 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/dashboard', function () {
+    return view('admin.dashboard.index');
+})->name('dashboard');
 Route::prefix('admin')->name('admin.')->group(function () {
-
-    Route::get('/', function () {
-        return view('admin.dashboard');
-    })->name('dashboard');
-
-    Route::get('/hotel', function () {
-        return view('admin.hotel.index');
-    })->name('hotel.index');
-
-    Route::get('/booking', function () {
-        return view('admin.booking.index');
-    })->name('booking.index');
+    //
 });
